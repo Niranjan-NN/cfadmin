7f5f8e459376044edcf4b7c6efe68533f65e394d
Niranjan NN <niranjan87901@gmail.com>
2025-08-25 15:12:09 +0530

    Added

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://cfadmin-five.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
